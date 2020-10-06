95bedd016431a0b363df252b0ae5c780908b5c9d
ricardo velasquez <riveca10@gmail.com>
2020-10-05 18:27:45 -0600

    docs: Add intro comment in bikeshare file

--- Bikeshare_RVC_v6.py
+++ Bikeshare_RVC_v6.py
@@ -1,7 +1,7 @@
 import time
 import pandas as pd
 import numpy as np
-
+# This is my first python script, hope you enjoy it!!!
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
